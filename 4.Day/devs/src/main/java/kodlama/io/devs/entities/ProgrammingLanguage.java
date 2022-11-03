66e5de90b76db8658afab8775bd97d1840a6411f
omerr02200 <omerr_02200@hotmail.com>
2022-11-03 09:38:29 +0300

    4.DayWork
    4.Day Kodlama.io.Devs

---
+++ 4.Day/devs/src/main/java/kodlama/io/devs/entities/ProgrammingLanguage.java
@@ -0,0 +1,27 @@
+package kodlama.io.devs.entities;
+
+public class ProgrammingLanguage {
+	int id;
+	String name;
+	
+	public ProgrammingLanguage(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
